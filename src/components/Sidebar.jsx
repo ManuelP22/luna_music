c8368a8f142449b525437bfb85f74f88592613b5
ManuelP2 <manuelperezp22@hotmail.com>
2022-11-30 11:44:29 -0400

    topPlay & artist frontend

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,14 +3,13 @@ import { NavLink } from 'react-router-dom';
 import { RiCloseLine } from 'react-icons/ri';
 import { HiOutlineMenu } from 'react-icons/hi';
 
-import { logo } from '../assets';
 import { links } from '../assets/constants';
 
 const NavLinks = ({ handleClick }) => (
   <div className="mt-10">
     {links.map((item) => (
       <NavLink key={item.name} to={item.to} onClick={() => handleClick && handleClick()} className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400">
-        <item.icon className="w-6 h-6 mr-2"/>
+        <item.icon className="w-6 h-6 mr-2" />
         {item.name}
       </NavLink>
     ))}
@@ -38,7 +37,7 @@ const Sidebar = () => {
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Sidebar;
