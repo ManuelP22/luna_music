b886aed19e66f93f087fc77647c5d876150c3d1a
ManuelP2 <manuelperezp22@hotmail.com>
2022-12-03 11:34:42 -0400

    AroundYou, Artist & Detail Ready

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Error = () => (
-  <div className="w-full flex justify-center items-center">
+  <div className="w-full flex flex-col justify-center items-center">
     <h1 className="font-bold text-xl text-white mt-2">Algo Salio mal. Porfavor intentelo denuevo</h1>
+    <h1 className="font-bold text-xl text-white mt-2">O su <span className="text-cyan-400">Pais</span> no cuenta con estas listas</h1>
   </div>
 );
 
